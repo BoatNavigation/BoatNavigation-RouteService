23eeaeed3611bd83dbededbea0bcab7a71b51b14
libsyfer <libsyfer@gmail.com>
2024-12-11 16:31:54 +0300

    Изменение названий базовых классов

---
+++ BoatNavigation-RouteService.Domain/NavigationRoute.cs
@@ -0,0 +1,21 @@
+﻿namespace BoatNavigation_RouteService.Domain
+{
+    public class NavigationRoute
+    {
+        public Guid Id { get; }
+
+        public List<NavigationRoutePoint> Points { get; set; }
+
+        public NavigationRoute()
+        {
+            Id = Guid.NewGuid();
+            Points = new List<NavigationRoutePoint>();
+        }
+
+        public NavigationRoute(Guid id)
+        {
+            Id = id;
+            Points = new List<NavigationRoutePoint>();
+        }
+    }
+}
