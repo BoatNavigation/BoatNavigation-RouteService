0767169dc50930410a25af5bd6143cd66a19f7b5
libsyfer <libsyfer@gmail.com>
2024-12-11 16:23:25 +0300

    Добавление объектов маршрута и точек маршрута

---
+++ BoatNavigation-RouteService.Domain/RouteData.cs
@@ -0,0 +1,21 @@
+﻿namespace BoatNavigation_RouteService.Domain
+{
+    public class RouteData
+    {
+        public Guid Id { get; }
+
+        public List<RoutePoint> Points { get; set; }
+
+        public RouteData()
+        {
+            Id = Guid.NewGuid();
+            Points = new List<RoutePoint>();
+        }
+
+        public RouteData(Guid id)
+        {
+            Id = id;
+            Points = new List<RoutePoint>();
+        }
+    }
+}
---
+++ BoatNavigation-RouteService.Domain/RoutePoint.cs
@@ -0,0 +1,11 @@
+﻿namespace BoatNavigation_RouteService.Domain
+{
+    public struct RoutePoint
+    {
+        public Guid Id { get; set; }
+
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+    }
+}
